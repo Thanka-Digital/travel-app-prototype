a89c180143adfa3947ffb0ee3dee34dbfa176bb4
utsab043 <grgutsab66@gmail.com>
2025-03-19 10:33:14 +0545

    Changes : Changes in plan Details page

--- src/pages/PlansPage/PlanDetails.tsx
+++ src/pages/PlansPage/PlanDetails.tsx
@@ -1,7 +1,6 @@
 import LocationCard from "../LocationPage/components/LocationCard";
 import Button from "@/components/form/button/Button";
 import BackButton from "@/components/global/BackButton";
-import MaxWidthWrapper from "@/layout/wrapper/MaxWidthWrapper";
 import { locationInfo } from "@/utils/locationDetailsData";
 import { tripPlanData } from "@/utils/tripPlanData";
 import { useNavigate } from "react-router-dom";
@@ -10,36 +9,33 @@ import { useParams } from "react-router-dom";
 export default function PlanPageDetails() {
   const navigate = useNavigate();
   const { id } = useParams();
-
   const plans = tripPlanData.find((t) => t.id === Number(id))
 
   return (
-    <div className="relative bg-white h-screen text-black">
+    <div className="flex flex-col justify-center items-center relative bg-white text-black pb-8">
       <BackButton />
-      <div className="bg-gradient-to-r from-indigo-500">
-        <img
-          src={plans?.image}
-          alt="location images"
-          height={50}
-        />
-      </div>
-      <MaxWidthWrapper>
-        <div className="my-4">
-          <p className="text-2xl font-semibold">
-            {plans?.name}
-          </p>
-          {plans?.isTripSuccess ? (
-            <p className="text-gray-400">
-              {plans.tripTimeStatus}
-            </p>
-          ) : (
-            <p className="font-medium text-danger">
-              {plans?.tripTimeStatus}
-            </p>
-          )}
-        </div>
+      <img
+        src={plans?.image}
+        alt="location images"
+        className="h-44 w-full object-cover"
+      />
 
-        <div className="flex flex-wrap gap-3 my-6">
+      <div className="max-w-[90%] mx-auto">
+        <div className='mask min-h-[100px] bg-white -mb-2' />
+        <p className="text-2xl font-medium">
+          {plans?.name}
+        </p>
+        {plans?.isTripSuccess ? (
+          <p className="text-gray-400">
+            {plans.tripTimeStatus}
+          </p>
+        ) : (
+          <p className="font-medium text-danger">
+            {plans?.tripTimeStatus}
+          </p>
+        )}
+
+        <div className="flex flex-wrap gap-3 py-4">
           {plans?.tripTypes.map((object) => (
             <GhostButton
               icon={object.icon}
@@ -49,8 +45,8 @@ export default function PlanPageDetails() {
           ))}
         </div>
 
-        <div>
-          <p className="text-xl font-semibold my-2">Location</p>
+        <div className="pb-6">
+          <p className="text-xl font-medium py-1">Location</p>
           {
             locationInfo.filter((l) => l.id === Number(id)).map((object) => (
               <LocationCard
@@ -60,18 +56,19 @@ export default function PlanPageDetails() {
             ))
           }
         </div>
-        <Button
-          onClick={() => navigate(`/trip-plan/details/${id}`)}
-          className={`w-full flex justify-center rounded-full py-3 text-white my-6 ${plans?.isTripSuccess ? "bg-primary" : "bg-black"
-            }`}
-        >
-          {plans?.isTripSuccess ? (
-            <p className="">Create a post for this trip?</p>
-          ) : (
-            <p className="">Replan this trip?</p>
-          )}
-        </Button>
-      </MaxWidthWrapper>
+      </div>
+
+      <Button
+        onClick={() => navigate(`${plans?.isTripSuccess ? "/add-post" : "/trip-plan/create"}/`)}
+        className={`rounded-3xl py-3 text-sm text-white ${plans?.isTripSuccess ? "bg-primary" : "bg-black"
+          }`}
+      >
+        {plans?.isTripSuccess ? (
+          <p>Create a post for this trip?</p>
+        ) : (
+          <p>Replan this trip?</p>
+        )}
+      </Button>
     </div>
   );
 }
@@ -84,7 +81,7 @@ export const GhostButton = (props: GhostButtonProps) => {
   const { icon, text } = props;
   return (
     <button
-      className="p-2 rounded-lg border border-primary text-primary transition duration-200"
+      className="p-2 rounded-lg border border-primary text-primary"
     >
       <span className="flex gap-1 items-center">
         {icon}
