ec9c01e424e55a7c575e8e6a6736f18ccb237a5f
Alson Garbuja <magar33alson@gmail.com>
2025-03-26 21:45:18 +0545

    fix: fix location data type issue

--- src/pages/PlansPage/PlanDetails.tsx
+++ src/pages/PlansPage/PlanDetails.tsx
@@ -9,30 +9,24 @@ import { useParams } from "react-router-dom";
 export default function PlanPageDetails() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const plans = tripPlanData.find((t) => t.id === Number(id))
+  const plans = tripPlanData.find((t) => t.id === Number(id));
 
   return (
-    <div className="flex flex-col justify-center items-center relative bg-white text-black pb-8">
+    <div className="relative flex flex-col items-center justify-center pb-8 text-black bg-white">
       <BackButton />
       <img
         src={plans?.image}
         alt="location images"
-        className="h-44 w-full object-cover"
+        className="object-cover w-full h-44"
       />
 
       <div className="max-w-[90%] mx-auto">
-        <div className='mask min-h-[100px] bg-white -mb-2' />
-        <p className="text-2xl font-medium">
-          {plans?.name}
-        </p>
+        <div className="mask min-h-[100px] bg-white -mb-2" />
+        <p className="text-2xl font-medium">{plans?.name}</p>
         {plans?.isTripSuccess ? (
-          <p className="text-gray-400">
-            {plans.tripTimeStatus}
-          </p>
+          <p className="text-gray-400">{plans.tripTimeStatus}</p>
         ) : (
-          <p className="font-medium text-danger">
-            {plans?.tripTimeStatus}
-          </p>
+          <p className="font-medium text-danger">{plans?.tripTimeStatus}</p>
         )}
 
         <div className="flex flex-wrap gap-3 py-4">
@@ -46,22 +40,24 @@ export default function PlanPageDetails() {
         </div>
 
         <div className="pb-6">
-          <p className="text-xl font-medium py-1">Location</p>
-          {
-            locationInfo.filter((l) => l.id === Number(id)).map((object) => (
-              <LocationCard
-                {...object}
-                key={object.id}
-              />
-            ))
-          }
+          <p className="py-1 text-xl font-medium">Location</p>
+          {locationInfo
+            .filter((l) => l.id === Number(id))
+            .map((object) => (
+              <LocationCard location={object} key={object.id} />
+            ))}
         </div>
       </div>
 
       <Button
-        onClick={() => navigate(`${plans?.isTripSuccess ? "/add-post" : "/trip-plan/create"}/`)}
-        className={`rounded-3xl py-3 text-sm text-white ${plans?.isTripSuccess ? "bg-primary" : "bg-black"
-          }`}
+        onClick={() =>
+          navigate(
+            `${plans?.isTripSuccess ? "/add-post" : "/trip-plan/create"}/`
+          )
+        }
+        className={`rounded-3xl py-3 text-sm text-white ${
+          plans?.isTripSuccess ? "bg-primary" : "bg-black"
+        }`}
       >
         {plans?.isTripSuccess ? (
           <p>Create a post for this trip?</p>
@@ -80,10 +76,8 @@ interface GhostButtonProps {
 export const GhostButton = (props: GhostButtonProps) => {
   const { icon, text } = props;
   return (
-    <button
-      className="p-2 rounded-lg border border-primary text-primary"
-    >
-      <span className="flex gap-1 items-center">
+    <button className="p-2 border rounded-lg border-primary text-primary">
+      <span className="flex items-center gap-1">
         {icon}
         <p className="text-sm">{text}</p>
       </span>
